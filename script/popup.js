f3f1078f16b956c1877daf6bfc1c44ad86e11ab2
maria tektova <tkmarie@yandex.ru>
2022-11-03 12:41:33 +0300

    didit

--- script/popup.js
+++ script/popup.js
@@ -1,38 +1,34 @@
-  let openElem = document.querySelector('.profile__info-button');
-  let popupElem = document.querySelector('.popup');
-  let closeElem = document.querySelector('.form__close');
-  let formElem = document.querySelector('.form');
-  let nameInput = formElem.querySelector('input[name="name"]');
-  let jobInput = formElem.querySelector('input[name="job"]');
-  let profileName = document.querySelector('.profile__info-name');
-  let profileText = document.querySelector('.profile__info-text');
+let openElem = document.querySelector(".profile__info-button");
+let popupElem = document.querySelector(".popup");
+let closeElem = document.querySelector(".popup__close");
+let formElem = document.querySelector(".form");
+let nameInput = formElem.querySelector('input[name="name"]');
+let jobInput = formElem.querySelector('input[name="job"]');
+let profileName = document.querySelector(".profile__info-name");
+let profileText = document.querySelector(".profile__info-text");
 
-  function openModal () {
-    popupElem.classList.add('popup_opened');
-  }
-  
-  
+function openModal() {
+  popupElem.classList.add("popup_opened");
+}
 
-  function closeModal () {
-    popupElem.classList.remove('popup_opened');
-  };
-  
+function closeModal() {
+  popupElem.classList.remove("popup_opened");
+}
 
-  function changeElem() {
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileText.textContent;
-  }
+function changeElem() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileText.textContent;
+}
 
-  function formSubmitHandler (elem) {
-	elem.preventDefault();
+function formSubmitHandler(elem) {
+  elem.preventDefault();
 
   profileName.textContent = nameInput.value;
   profileText.textContent = jobInput.value;
 
   closeModal();
-
-};
+}
 changeElem();
-openElem.addEventListener('click', openModal);
-formElem.addEventListener('submit',formSubmitHandler)
-closeElem.addEventListener('click', closeModal);
+openElem.addEventListener("click", openModal);
+formElem.addEventListener("submit", formSubmitHandler);
+closeElem.addEventListener("click", closeModal);
